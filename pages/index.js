32055578f50ac1da886da93f4519805c77abb2bd
Duong Chien <duongvanchien1003@gmail.com>
2021-04-28 17:34:42 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Demo</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
