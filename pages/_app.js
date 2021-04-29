bfb1e02ff2d8f236ed9c7178c9e174da9788185b
Duong Chien <duongvanchien1003@gmail.com>
2021-04-29 18:03:44 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,14 @@ import "antd/dist/antd.css";
 import { wrapper } from "../store/store";
 
 // import Reactotron from "reactotron-react-js";
+// if (!__SERVER__) {
+//   const Reactotron = require('reactotron-react-js');
+//   Reactotron.configure() // we can use plugins here -- more on this later
+//     .connect(); // let's connect!
+// }
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-
-// Reactotron.configure() // we can use plugins here -- more on this later
-//   .connect(); // let's connect!
 
 export default wrapper.withRedux(MyApp);
--- store/user/reducer.js
+++ store/user/reducer.js
@@ -7,7 +7,6 @@ const initialState = {
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case userActionTypes.SUBMIT_USERNAME:
-      console.log(action);
       return { ...initialState, name: action.name };
     default:
       return state;
